108a68a7b9481ac40c1b19d257d528a5fa818d54
96kitten <niko05139625@gmail.com>
2019-05-12 16:00:54 +0900

    2019/5/12 主人公の追加　terrain asset追加

---
+++ Assets/scripts/FiaryMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiaryMove : MonoBehaviour {
+	float speed = 0.4f;
+	int rotespeed= 2;
+
+	private Animator Fairymove;
+
+	// Use this for initialization
+	void Start () {
+		Fairymove = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//主人公の移動
+		Fairymove.SetBool ("is_walking", false);
+		if (Input.GetKey (KeyCode.W)) {
+			this.transform.Translate (Vector3.forward * speed);
+			Fairymove.SetBool ("is_walking", true);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			this.transform.Translate(Vector3.forward * -speed);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (0,rotespeed,0);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (0,-rotespeed,0);
+		}
+	}
+}
